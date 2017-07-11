b09aa2f2b77b90330a6105fd08657ce2e536a542
Shihan <g3aishih@gmail.com>
2017-07-11 15:40:30 -0400

    improved contour selection, added new image metric

--- run.py
+++ run.py
@@ -23,9 +23,6 @@ class ForegroundExtraction:
         self.fDown = [self.foregroundSeed]
         self.bDown = [self.backgroundSeed]
 
-        # Kernel used for convolution
-        self.kernel = np.ones((5,5), np.uint8)
-
         # Keep a list of contour points for items being cut out
         self.cuts = None
 
@@ -41,6 +38,7 @@ class ForegroundExtraction:
 
         # Images stored for debugging purposes
         self._contour = None
+        self._contourSource = None
         self._initialMask = None
         self._refinedMask = np.zeros(self.source.shape[:2], dtype=np.uint8)
 
@@ -49,13 +47,16 @@ class ForegroundExtraction:
         self.minImageSize = 500000
         self.timesDownsampled = 0
 
-        # Hyper parameters to play with
-        self.erodeIterations = 5
-        self.dilateIterations = 5
-        self.patchRadius = 25
+        # Kernel used for convolution
+        self.kernel = np.ones((3,3), np.uint8)
+
+        # Hyper parameters to tweak
+        self.erodeIterations = 8
+        self.dilateIterations = 8
+        self.patchRadius = 10
         self.colorWeight = 0.5
         self.locationWeight = 0.5
-        self.contourSizeThreshold = 500
+        self.contourSizeThreshold = int(source.shape[0] * source.shape[1] * 0.05)
 
     def run(self):
         # We down sample the large image to a much more managable size
@@ -63,7 +64,7 @@ class ForegroundExtraction:
         # esimate of the cutout mask. Using this estimate, we can obtain a more
         # refined foreground and background seed.
         self.downSample()
-        self.cuts, self.foreground, self.background, self._contour = self.getBoundaries()
+        self.cuts, self.foreground, self.background, self._contour , self._contourSource = self.getBoundaries()
 
         # Using the refined foreground and background seeds, we take the estimated
         # cutout mask and refine the contour by examining patches that lie on the contour
@@ -72,7 +73,7 @@ class ForegroundExtraction:
 
         # Cut out the original image using the refined mask
         self.cutOut[self._refinedMask==0] = 0
-        return self.cutOut, self._refinedMask, self._contour, self._initialMask
+        return self.cutOut, self._refinedMask, self._contour, self._contourSource, self._initialMask
 
     # Constructs an image pyramid by scaling down the image and foreground and background seeds
     def downSample(self):
@@ -121,33 +122,43 @@ class ForegroundExtraction:
 
         # Create image representations of the new foreground and background seeds
         contour = np.ones(self.source.shape, dtype=np.uint8) * 255
+        contourSource = self.source.copy()
         foreground = np.zeros(self.source.shape[:2], dtype=np.uint8)
         background = np.zeros(self.source.shape[:2], dtype=np.uint8)
 
         cuts = []
         self.numCuts = min((len(boundary[0]), len(erodedBoundary[0]), len(dilatedBoundary[0])))
-        for i in range(1, self.numCuts + 1):
-            points = boundary[0][-i]
-            erodedPoints = erodedBoundary[0][-i]
-            dilatedPoints = dilatedBoundary[0][-i]
 
-            # Red
-            for p in points:
-                contour[p[0][1], p[0][0]] = [0, 0, 255]
+        # Only deal with the largest contours to avoid artifacts
+        biggestContours = np.array([cv.contourArea(x) for x in boundary[0]]).argsort()[-self.numCuts:]
+        biggestErodedContours = np.array([cv.contourArea(x) for x in erodedBoundary[0]]).argsort()[-self.numCuts:]
+        biggestDilatedContours = np.array([cv.contourArea(x) for x in dilatedBoundary[0]]).argsort()[-self.numCuts:]
 
-            # Green
-            for p in erodedPoints:
-                foreground[p[0][1], p[0][0]] = 255
-                contour[p[0][1], p[0][0]] = [0, 255, 0]
+        for i in range(0, self.numCuts):
+            points = boundary[0][biggestContours[i]]
+            erodedPoints = erodedBoundary[0][biggestErodedContours[i]]
+            dilatedPoints = dilatedBoundary[0][biggestDilatedContours[i]]
 
-            # Blue
-            for p in dilatedPoints:
-                background[p[0][1], p[0][0]] = 255
-                contour[p[0][1], p[0][0]] = [255, 0, 0]
+            if (cv.contourArea(points) > self.contourSizeThreshold):
+                # Red
+                for p in points:
+                    contour[p[0][1], p[0][0]] = [0, 0, 255]
+                    contourSource[p[0][1], p[0][0]] = [0, 0, 255]
 
-            cuts.append((points, erodedPoints, dilatedPoints))
+                # Green
+                for p in erodedPoints:
+                    foreground[p[0][1], p[0][0]] = 255
+                    contour[p[0][1], p[0][0]] = [0, 255, 0]
+                    contourSource[p[0][1], p[0][0]] = [0, 255, 0]
 
-        return cuts, foreground, background, contour
+                # Blue
+                for p in dilatedPoints:
+                    background[p[0][1], p[0][0]] = 255
+                    contour[p[0][1], p[0][0]] = [255, 0, 0]
+                    contourSource[p[0][1], p[0][0]] = [255, 0, 0]
+
+                cuts.append((points, erodedPoints, dilatedPoints))
+        return cuts, foreground, background, contour, contourSource
 
     # Refines the contour by examining patches on the contour
     def refineBoundary(self, cuts):
@@ -311,23 +322,26 @@ def main(args):
 
     # Run algorithm
     extraction = ForegroundExtraction(source, foregroundSeed, backgroundSeed)
-    result, _refinedMask, _contour, _initialMask = extraction.run()
+    result, _refinedMask, _contour, _contourSource, _initialMask = extraction.run()
 
     # Figure out the paths for the debugging images
     splitString = args.o.split('.')
     _refinedMaskPath = str(splitString[0]) + '_refinedMask.'
     _contourPath = str(splitString[0]) + '_contour.'
+    _contourSourcePath = str(splitString[0]) + '_contourSource.'
     _initialMaskPath = str(splitString[0]) + '_initialMask.'
 
     for i in range(1, len(splitString)):
         _refinedMaskPath += str(splitString[i])
         _contourPath += str(splitString[i])
+        _contourSourcePath += str(splitString[i])
         _initialMaskPath += str(splitString[i])
 
     # Write the images
     writeImage(args.o, result)
     writeImage(_refinedMaskPath, _refinedMask)
     writeImage(_contourPath, _contour)
+    writeImage(_contourSourcePath, _contourSource)
     writeImage(_initialMaskPath, _initialMask)
 
 if __name__ == '__main__':
